9eb231c212281847d951ab964baf5b0f087080e3
AKArunkumar <arunkumar717.at@gmail.com>
2018-05-06 12:05:00 +0530

    created model for each item

--- src/app/models/item.ts
+++ src/app/models/item.ts
@@ -1,2 +1,13 @@
 export class Item {
+    private id: number;
+    private name: string;
+    private imageUrl: string;
+    private price: number;
+
+    constructor(id: number, name: string, imageUrl: string, price: number) {
+        this.id = id;
+        this.name = name;
+        this.imageUrl = imageUrl;
+        this.price = price;
+    }
 }
