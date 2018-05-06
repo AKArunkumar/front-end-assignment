761cd95d65321d261de131affc8f00bb941c8fff
AKArunkumar <arunkumar717.at@gmail.com>
2018-05-06 12:05:39 +0530

    added dummy data, get function

--- src/app/services/items.service.ts
+++ src/app/services/items.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@angular/core';
 
+import { Item } from '../models/item';
+
 @Injectable()
 export class ItemsService {
+  shopItems: Item[] = [
+    new Item(1, "Item1", "http://keyassets.timeincuk.net/inspirewp/live/wp-content/uploads/sites/34/2012/05/000003056-bollinger.jpg", 100),
+    new Item(2, "Itme2", "http://keyassets.timeincuk.net/inspirewp/live/wp-content/uploads/sites/34/2012/05/000003056-bollinger.jpg", 100),
+    new Item(3, "Item3", "http://keyassets.timeincuk.net/inspirewp/live/wp-content/uploads/sites/34/2012/05/000003056-bollinger.jpg", 100),
+    new Item(4, "Item4", "http://keyassets.timeincuk.net/inspirewp/live/wp-content/uploads/sites/34/2012/05/000003056-bollinger.jpg", 100),
+    new Item(5, "Item4", "http://keyassets.timeincuk.net/inspirewp/live/wp-content/uploads/sites/34/2012/05/000003056-bollinger.jpg", 100),
+    new Item(6, "Item4", "http://keyassets.timeincuk.net/inspirewp/live/wp-content/uploads/sites/34/2012/05/000003056-bollinger.jpg", 100)
+  ]
 
   constructor() { }
+
+  getItems() {
+    return this.shopItems.slice();
+  }
 
 }
