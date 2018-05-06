b383702422cffb35eee8f1614dde821034dabf4b
AKArunkumar <arunkumar717.at@gmail.com>
2018-05-06 22:29:39 +0530

    getItem fuction() for cart

--- src/app/services/items.service.ts
+++ src/app/services/items.service.ts
@@ -5,12 +5,12 @@ import { Item } from '../models/item';
 @Injectable()
 export class ItemsService {
   shopItems: Item[] = [
-    new Item(1, "Item1", "../../assets/bottle.png", 1000),
-    new Item(2, "Itme2", "../../assets/bottle.png", 1000),
-    new Item(3, "Item3", "../../assets/bottle.png", 1000),
-    new Item(4, "Item4", "../../assets/bottle.png", 1000),
-    new Item(5, "Item4", "../../assets/bottle.png", 1000),
-    new Item(6, "Item4", "../../assets/bottle.png", 1000)
+    new Item(1, "Bottle", "../../assets/bottle.png", 100),
+    new Item(2, "Bottle", "../../assets/bottle.png", 100),
+    new Item(3, "Bottle", "../../assets/bottle.png", 100),
+    new Item(4, "Bottle", "../../assets/bottle.png", 100),
+    new Item(5, "Bottle", "../../assets/bottle.png", 100)
+    
   ]
 
   constructor() { }
